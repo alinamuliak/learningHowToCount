966d0c3f40623beb1c5941f7a5723e8c43ffea13
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2021-09-20 16:52:48 +0300

    reformat file

--- solutions.py
+++ solutions.py
@@ -1,8 +1,11 @@
 import time
+
+
 def print_slow(text: str) -> None:
     for symbol in text:
         print(symbol, flush=True, end='')
         time.sleep(0.1)
+
 
 def find_solution(numbers: list, problem_type: str) -> str:
     problems_types = {'P': 'n!',
@@ -29,19 +32,21 @@ def find_solution(numbers: list, problem_type: str) -> str:
         return -1
     return result_formula
 
+
 def check_user_answer(correct_answer: str) -> bool:
     user_answer = input('Enter your answer (for example: 5!, 25! / 20!): ')
     if correct_answer.replace(' ', '') == user_answer.replace(' ', ''):
         return True
     return False
 
+
 def generate_solution(numbers: list, problem_type: str, result_formula: str):
     problems_types = {'P': 'permutation',
-    'Ay': 'allocation with repetition',
-    'An': 'allocation without repetition',
-    'M': 'permutation of n objects with k1, k2, ...  indistinguishable objects',
-    'C': 'unordered group combinations',
-    'B': 'basic principle of counting'}
+                      'Ay': 'allocation with repetition',
+                      'An': 'allocation without repetition',
+                      'M': 'permutation of n objects with k1, k2, ...  indistinguishable objects',
+                      'C': 'unordered group combinations',
+                      'B': 'basic principle of counting'}
 
     return f'SOLUTION:\n{result_formula}\n\nIn such types of problems, \
 {problems_types[problem_type]} should be used. The common formula is as follows: {problems_types[problem_type]}'
